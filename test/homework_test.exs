4b5ef94c6dd3d224e05427ae4059067ee9fee0aa
huynxloi <huynhxloi@gmail.com>
2022-04-07 16:25:03 -0500

    Elixir SDET homework assigment

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -1,3 +1,9 @@
+# Author: Loi Huynh
+
+# Senior Technical Recruiter: Tyler Mills
+
+# Date: 4/7/22
+
 defmodule HomeworkTest do
   # Import helpers
   use Hound.Helpers
@@ -6,7 +12,47 @@ defmodule HomeworkTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "http://google.com"
+  test "Add/Remove Elements: Add Elements button should be visible" do
+    navigate_to "https://the-internet.herokuapp.com"
+    find_element(:link_text, "Add/Remove Elements") |> click()
+    element = find_element(:class, "example")
+    assert visible_text(element) == "Add Element"
   end
+
+  test "Able to select Dropdown List Option 2" do
+    navigate_to "https://the-internet.herokuapp.com"
+    find_element(:link_text, "Dropdown") |> click()
+    find_element(:id, "dropdown") |> click()
+    find_element(:css,  "option[value='1']") |> click()
+  end
+
+  test "Display invalid username message" do
+    navigate_to "https://the-internet.herokuapp.com"
+    find_element(:link_text, "Form Authentication") |> click()
+    input_into_field({:id, "username"}, "LoiHuynh")
+    input_into_field({:id, "password"}, "secret")
+    find_element(:css,  "button[type='submit']") |> click()
+    assert element_displayed?({:class, "error"})
+
+    path = take_screenshot("login-error.png")
+    assert File.exists?(path)
+  end
+
+  test "Type the TAB key and get result" do
+    navigate_to "https://the-internet.herokuapp.com"
+    find_element(:link_text, "Key Presses") |> click()
+    find_element(:id, "target") |> click()
+    send_keys(:tab)
+
+    element = find_element(:id, "result")
+    assert visible_text(element) == "You entered: TAB"
+  end
+
+  test "Type the TAB key and get result" do
+    url = "https://reqres.in/api/users?page=2"
+
+    response = HTTPoison.get!(url)
+    assert response.status_code == 200
+  end
+
 end
